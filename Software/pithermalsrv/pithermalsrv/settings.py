47ba01b7a224bf4efd9561bb81d96156ad1ba38b
Ángel González <aglezabad@gmail.com>
2017-07-24 14:14:34 +0200

    Added models for dashboard app

--- Software/pithermalsrv/pithermalsrv/settings.py
+++ Software/pithermalsrv/pithermalsrv/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'pithermalsrv.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'pithermal-db.sqlite3'),
     }
 }
 
