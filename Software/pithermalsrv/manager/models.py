61f75859fa1eb94eff53234d07203d647f52cf8f
Ángel González <aglezabad@gmail.com>
2017-07-26 13:57:16 +0200

    Setting custom names for object instances (Control node, sensor node, target

--- Software/pithermalsrv/manager/models.py
+++ Software/pithermalsrv/manager/models.py
@@ -3,13 +3,18 @@ from django.db import models
 # Create your models here.
 
 class TargetSetting(models.Model):
-    temperature = models.PositiveSmallIntegerField(default=0)
+    TEMP_UNIT_CHOICES = (
+        ('ºF', 'Fahrenheit'),
+        ('ºC', 'Celsius')
+    )
+    temperature = models.PositiveSmallIntegerField('Temperature', default=0)
+    temp_unit = models.CharField('Temperature unit', max_length=2, choices=TEMP_UNIT_CHOICES, default='ºC')
     from_time = models.TimeField('From')
     until_time = models.TimeField('Until')
     priority = models.PositiveSmallIntegerField(default=0)
     
     def __str__(self):
-        return str(self.temperature) + ' / ' + \
+        return str(self.temperature) + self.temp_unit + ' / ' + \
             self.from_time.strftime('%H:%M') + ' - ' + \
             self.until_time.strftime('%H:%M') + ' / ' + \
             str(self.priority)
@@ -17,7 +22,7 @@ class TargetSetting(models.Model):
 class Node(models.Model):
     mac_address = models.CharField(max_length=17)
     location = models.CharField(max_length=255)
-    last_query = models.DateTimeField('Last date queried', editable=False)
+    last_query = models.DateTimeField('Last date queried', auto_now_add=True, editable=False)
     temperature = models.PositiveSmallIntegerField(default=0, editable=False)
     humidity = models.PositiveSmallIntegerField(default=0, editable=False)
     valid = models.BooleanField(default=False, editable=False)
@@ -25,6 +30,9 @@ class Node(models.Model):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return self.mac_address + ' - ' + self.location
 
 class ControlNode(Node):
     pass
--- Software/pithermalsrv/pithermalsrv/urls.py
+++ Software/pithermalsrv/pithermalsrv/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^manager/', include('manager.urls')),
+    url(r'^', include('manager.urls')),
     url(r'^admin/', admin.site.urls),
 ]
